f0b8b701a7d9ed2ac5c5cce0f8ae1a9b868d5f53
TimD <TimD.Smith@wnco.com>
2026-04-15 09:51:04 -0400

    Adding Oil plugin.

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -228,6 +228,22 @@ local plugin_specs = {
       require("config.nvim_ufo")
     end,
   },
+  -- File Explorer
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    config = function()
+      require("config.oil")
+    end,
+  },
+
   -- Highlight URLs inside vim
   { "itchyny/vim-highlighturl", event = "BufReadPost" },
 
